2c2eccb0c4e794c26837a2ea0c36331fbc3cdaef
aldenezy <aldeneh@Zakarias-MacBook-Pro.local>
2016-02-27 17:20:44 -0500

    minor updates

---
+++ classification/classifySpeakers.m
@@ -0,0 +1,29 @@
+function results = classifySpeakers(speakers_in)
+
+    % get number of speakers
+    numSpeakers = numel(speakers_in);
+
+    % ------------------------------------------------------------------------
+    % get stats results
+    % ------------------------------------------------------------------------
+    results_stats = zeros(numSpeakers)
+
+    
+    for i=1:numSpeakers
+
+
+
+
+    end
+
+
+    keyboard
+
+
+
+
+
+
+
+
+end
--- run.m
+++ run.m
@@ -2,9 +2,9 @@
 % NP = str2num(getenv('PBS_NP'));
 % myPool = parpool('current', NP);
 % start parallel pool
-myCluster = parcluster('local');
-myCluster.NumWorkers = 12;
-parpool;
+% myCluster = parcluster('local');
+% myCluster.NumWorkers = 12;
+% parpool;
 
 % define input values
 dataInputFile = 'emoDB_IS2009_size0.5_step0.25';
@@ -16,6 +16,9 @@ diary(['diaries/', dataInputFile, '_', emotogramFunName, '_', notes, '.txt'])
 diary on
 
 % call main function
-results = main(dataInputFile, emotogramFunName, notes);
+speakers = main(dataInputFile, emotogramFunName, notes);
+
+% call classification code
+results = classifySpeakers(speakers);
 
 diary off
