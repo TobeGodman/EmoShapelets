c436cf9f7fc36e95458107240cf0e4ba12b5e297
aldenezy <aldeneh@Zakarias-MacBook-Pro.local>
2016-02-27 15:38:11 -0500

    added startup file

--- run.m
+++ run.m
@@ -6,19 +6,12 @@ myCluster = parcluster('local');
 myCluster.NumWorkers = 12;
 parpool;
 
-% add current directory to paths
-p = genpath(pwd); addpath(p);
-ORIG_PATH = path;
-
 % define input values
 dataInputFile = 'emoDB_IS2009_size0.5_step0.25';
 emotogramFunName = 'RF';
 notes = '';
 
 % add directories
-if ~exist('diaries', 'dir')
-    mkdir('diaries')
-end
 diary(['diaries/', dataInputFile, '_', emotogramFunName, '_', notes, '.txt'])
 diary on
 
@@ -26,6 +19,3 @@ diary on
 results = main(dataInputFile, emotogramFunName, notes);
 
 diary off
-
-% set original path
-path(ORIG_PATH);
